ef1fb3a33baa8ca98e8ca65c6400b445793fa80e
German Rojo <grojo@techk.cl>
2022-04-26 12:27:58 -0400

    feat: Add Hello World view

--- petclub/views.py
+++ petclub/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response(data="Hello, World !", status=200)
