5ec6cfce9d2d2600ab814a44481a12906f5d8f85
German Rojo <grojo@techk.cl>
2022-05-04 18:32:13 -0400

    feat: new urls for exaple

--- petclub/views.py
+++ petclub/views.py
@@ -1,6 +1,30 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import (
+    RetrieveAPIView,
+    ListAPIView,
+)
+
 
 class HelloWorld(APIView):
     def get(self, request):
-        return Response(data="Hello, World !", status=200)
+        return Response(data="Hola gente estoy en el get", status=200)
+
+    def patch(self, request):
+        return Response(data="Hola gente estoy en el patch", status=200)
+
+    def delete(self, request):
+        return Response(data="Hola gente estoy en el delete", status=200)
+
+    def post(self, request):
+        return Response(data="Hola gente estoy en el post", status=200)
+
+class PetListAPIView(ListAPIView):
+
+    def get(self, request):
+        return Response(data="Hola a todos estas son mis mascotas", status = 200)
+
+class PetAPIView(RetrieveAPIView):
+
+    def get(self, request):
+        return Response(data="Hola gente estoy en el post", status=200)
