3ee8821c21a55212abc26cf5204b09f2a802bbb4
German Rojo <grojo@techk.cl>
2022-05-04 18:34:05 -0400

    feat: Add owner field to Pet

--- petclub/models.py
+++ petclub/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Pet(models.Model):
+    id = models.AutoField(primary_key=True)
+    species = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    color = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    owner = models.ForeignKey(
+        'Person',
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,17 +21,9 @@ class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     rut = models.CharField(max_length=12)
+    age = models.IntegerField(null=True, blank=True)
 
     class Meta:
         managed = True
 
 
-class Pet(models.Model):
-    id = models.AutoField(primary_key=True)
-    species = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    age = models.IntegerField()
-    color = models.CharField(max_length=50)
-
-    class Meta:
-        managed = True
