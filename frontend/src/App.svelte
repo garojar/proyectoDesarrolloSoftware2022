cc682c8fa217685bf80a71fd6ae70d7a1c7215d5
Germán Rojo Arce <german.rojo@ce.ucn.cl>
2022-06-08 15:55:53 -0400

    feat: add routing to project with a few examples

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,72 +1,17 @@
 <script>
-    import {
-        Alert,
-        Container,
-        Styles,
-        Button,
-        Table,
-    } from 'sveltestrap';
-
-    let header = "Probando svelte";
-    let posts = [];
-    let renderLocalAPI = false;
-    let responseData = [];
-
-    const cambiarNombre = () => {
-        header = `Titulo ${counter}`;
-        counter += 1;
-    }
-
-
-    const getPosts = async () => {
-        renderLocalAPI = false;
-        const url = 'https://jsonplaceholder.typicode.com/posts';
-        const config = {
-            method: 'GET'
-        };
-        const response = await fetch(url,config);
-        responseData = await response.json();
-        posts = responseData;
-    };
-
-    const getPersons = async () => {
-        renderLocalAPI = true;
-        const url = 'http://0.0.0.0:8000/persons/';
-        const config = {
-            method: 'GET'
-        };
-        const response = await fetch(url,config);
-        responseData = await response.json();
-        console.log(responseData);
-    }
-
-    const showAlert = () => alertStates.test = !alertStates.test;
+    import { Router, Link, Route } from "svelte-routing";
+    import Home from './routes/Home.svelte';
 </script>
-<Styles/>
 
-<main>
-<Container>
-        <h1>{header}</h1>
-        <p> Este es un elemento p</p>
-        <Table bordered>
-            <thead>
-              <tr>
-                <th>{ renderLocalAPI ? 'id' : 'UserId'}</th>
-                <th>{ renderLocalAPI ? 'Name' : 'id'}</th>
-                <th>{ renderLocalAPI ? 'Rut' : 'title'}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {#each responseData as data}
-                <tr>
-                    <td>{ renderLocalAPI ? data.id : data.userId}</td>
-                    <td>{ renderLocalAPI ? data.first_name : data.id}</td>
-                    <td>{ renderLocalAPI ? data.rut : data.title}</td>
-                </tr>
-              {/each}
-            </tbody>
-          </Table>
-        <Button color='primary' on:click={getPosts}> Obtener posts</Button>
-        <Button color='primary' on:click={getPersons}> Obtener personas (API LOCAL)</Button>
-</Container>
-</main>
+<div>
+	<h1>App</h1>
+	<Router>
+		<Link to="/home">Home</Link>
+		<Link to="/login">Login</Link>
+		<Link to="/profile">Perfil</Link>
+
+		<Route path="/home" component={Home}/>
+		<Route path="/login" />
+		<Route path="/profile" />
+	</Router>
+</div>
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {
+        Container
+    } from 'sveltestrap';
+
+</script>
+
+<Container>
+    <h1>Home page</h1>
+</Container>
